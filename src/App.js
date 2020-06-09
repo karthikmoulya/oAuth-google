e70197d0a0621797384c4a10f2b0743e1423f33b
Karthik Moulya <karthik@pop-os.localdomain>
2020-06-09 16:16:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import GoogleLogin from 'react-google-login';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [url, setUrl] = useState('');
+
+  const responseGoogle = response => {
+    setName(response.profileObj.name);
+    setEmail(response.profileObj.email);
+    setUrl(response.profileObj.imageUrl);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login with google</h1>
+      <h2>Welcome: {name}</h2>
+      <h2>Email: {email}</h2>
+      <img src={url} alt={name} />
+      <GoogleLogin
+        clientId="132820051373-gj77pgh81852kpivlun8fauk819ffn7a.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
-}
+};
 
 export default App;
